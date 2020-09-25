67e8c6fe1bf3ba31fbf595f33bb0d191b2305d25
Barsik4UbZik <nik.chisr@gmail.com>
2020-09-25 21:08:14 +0300

    Yes

---
+++ src/com/Pr3/TestBook.java
@@ -0,0 +1,10 @@
+package com.Pr3;
+
+public class TestBook
+{
+    public static void main(String[] args)
+    {
+        Book book1 = new Book("Loshok", "A.B.Covid", 1337);
+        System.out.println(book1.toString());
+    }
+}
