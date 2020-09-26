ef40c54c1d6a950387155c10ce21545a7f3be4b8
Barsik4UbZik <nik.chisr@gmail.com>
2020-09-26 10:42:32 +0300

    Yes

--- src/com/Pr6/MovablePoint.java
+++ src/com/Pr6/MovablePoint.java
@@ -2,5 +2,28 @@ package com.Pr6;
 
 public class MovablePoint
 {
+    private int x;
+    private int y;
+    public MovablePoint (int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
     
 }
