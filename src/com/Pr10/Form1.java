b81e7a12a18d5cedd6327bcedf5883b287293a29
Barsik4UbZik <Korol-Barsik@yandex.ru>
2020-10-10 09:48:01 +0300

    Yes

--- src/com/Pr10/Form1.java
+++ src/com/Pr10/Form1.java
@@ -1,4 +1,47 @@
 package com.Pr10;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class Form1 {
+    private JButton button1;
+    private JPanel panel1;
+    private JTextArea textArea1;
+
+    public JButton getButton1()
+    {
+        return button1;
+    }
+
+    public JPanel getPanel1()
+    {
+        return panel1;
+    }
+
+    public JTextArea getTextArea1()
+    {
+        return textArea1;
+    }
+
+    Form1()
+    {
+        button1.addActionListener(new ActionListener());
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.println(textArea1.getText());
+            }
+        }
+    };
+
+    textArea1.addMouseListener(new MouseListener())
+    {
+        @Override
+        public void mouseClicked(MouseEvent e)
+        {
+            textArea1.setBackground(Color.magenta);
+        }
+    }
 }
--- src/com/Pr10/Loader.java
+++ src/com/Pr10/Loader.java
@@ -6,18 +6,20 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame(); // Создание окна
         frame.setVisible(true);
-        frame.setSize(480,320); // Размер окна
+        frame.setSize(1080,720); // Размер окна
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Выход при закрытии окна
 
         frame.setLocationRelativeTo(null);
-        frame.setLayout(null);
+        //frame.setLayout(null);
 
-        JButton button = new JButton("Lox"); // Кнопка с надписью
-        frame.add(button); // Создание кнопки
+        //JButton button = new JButton("Lox"); // Кнопка с надписью
+        //frame.add(button); // Создание кнопки
 
-        button.setBounds(10, 10, 100, 30); // Положение кнопки
+        //button.setBounds(10, 10, 100, 30); // Положение кнопки
+
+        frame.add(new Form1().getPanel1()); // Импорт из Form1
     }
 }
