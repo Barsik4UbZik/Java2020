1f2d291a57c3a704a232adbf946945ae28bfaffc
Barsik4UbZik <nik.chisr@gmail.com>
2020-11-03 14:00:53 +0300

    Yes

--- src/com/Pr7_8/Main.java
+++ src/com/Pr7_8/Main.java
@@ -9,8 +9,8 @@ public class Main
     public static void main(String[] args)
     {
         Company company = new Company();
-        Employee emp = new Employee("Last", "Name", 150, null);
-        Employee emp2 = new Employee("Last2", "Name2", 1500, null);
+        Employee emp = new Employee("Lox", "Net", 1500, null);
+        Employee emp2 = new Employee("Lashok", "Nitu", 15000, null);
 
         System.out.println(company);
         company.hire(emp, new Manager(1000));
